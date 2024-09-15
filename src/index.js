d323749caacda67e5acc2562bfc00952c1bc2705
Ana Carolina <karols.15@hotmail.com>
2024-09-15 14:02:45 -0300

    feat: dealing with colors

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoute'); 
-
+const cors = require('cors'); 
 const app = express();
 
-
+app.use(cors({
+    origin: 'http://localhost:3000', // Permite requisições da aplicação React
+  }));
+  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
